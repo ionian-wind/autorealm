5ca2eff3b4c06907b3f67fa0f8cd3c9b9d510169
bmorel <berenger.morel@neutralite.org>
2012-04-14 15:23:44 +0200

    Add licence text on files where it were needed.

--- src/pluginEngine/plugin.h
+++ src/pluginEngine/plugin.h
@@ -1,6 +1,27 @@
+/**********************************************************************************
+ *autorealm - A vectorized graphic editor to create maps, mostly for RPG games    *
+ *Copyright (C) 2012 Morel Bérenger                                               *
+ *                                                                                *
+ *This file is part of autorealm.                                                 *
+ *                                                                                *
+ *    autorealm is free software: you can redistribute it and/or modify           *
+ *    it under the terms of the GNU General Public License as published by        *
+ *    the Free Software Foundation, either version 3 of the License, or           *
+ *    (at your option) any later version.                                         *
+ *                                                                                *
+ *    autorealm is distributed in the hope that it will be useful,                *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ *    GNU General Public License for more details.                                *
+ *                                                                                *
+ *    You should have received a copy of the GNU General Public License           *
+ *    along with autorealm.  If not, see <http://www.gnu.org/licenses/>.          *
+ **********************************************************************************/
+
 #ifndef PLUGIN_H
 #define PLUGIN_H
 
+#include <string>
 
 class Plugin
 {
@@ -9,6 +30,7 @@ class Plugin
 		virtual ~Plugin();
 	protected:
 	private:
+		static const std::string m_configFileName;
 };
 
 #endif // PLUGIN_H
--- src/unimplemented.h
+++ src/unimplemented.h
@@ -1,3 +1,23 @@
+/**********************************************************************************
+ *autorealm - A vectorized graphic editor to create maps, mostly for RPG games    *
+ *Copyright (C) 2012 Morel Bérenger                                               *
+ *                                                                                *
+ *This file is part of autorealm.                                                 *
+ *                                                                                *
+ *    autorealm is free software: you can redistribute it and/or modify           *
+ *    it under the terms of the GNU General Public License as published by        *
+ *    the Free Software Foundation, either version 3 of the License, or           *
+ *    (at your option) any later version.                                         *
+ *                                                                                *
+ *    autorealm is distributed in the hope that it will be useful,                *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ *    GNU General Public License for more details.                                *
+ *                                                                                *
+ *    You should have received a copy of the GNU General Public License           *
+ *    along with autorealm.  If not, see <http://www.gnu.org/licenses/>.          *
+ **********************************************************************************/
+
 #include <stdexcept>
 
 #define UNIMPLEMENTED throw std::logic_error("not implemented yet")
