a5fc959aea23539e524bf746f5bb4b3dee71be20
Morel Bérenger <berenger.morel@neutralite.org>
2012-04-17 19:46:14 +0200

    Allow Item to be associated with a plug-in

--- src/menuEngine/item.cpp
+++ src/menuEngine/item.cpp
@@ -1,11 +1,16 @@
 #include "item.h"
 
 Item::Item(boost::filesystem::path const &location)
-:MenuItem()
+:MenuItem(), m_plugin()
 {
 }
 
 Item::~Item()
 {
 	//dtor
+}
+
+void Item::associate(std::unique_ptr<Plugin> target)
+{
+	m_plugin.reset(target);
 }
--- src/menuEngine/item.h
+++ src/menuEngine/item.h
@@ -1,17 +1,26 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <memory>
+
 #include "menuitem.h"
 
+class Plugin;
 
 class Item : public MenuItem
 {
-	friend class Menu;
-	public:
-		virtual ~Item();
-	protected:
-		Item(boost::filesystem::path const &location);
-	private:
+friend class Menu;
+public:
+	virtual ~Item();
+	void associate(std::unique_ptr<Plugin> target);
+protected:
+	Item(boost::filesystem::path const &location);
+private:
+
+public:
+protected:
+	std::unique_ptr<Plugin> m_plugin;
+private:
 };
 
 #endif // ITEM_H
