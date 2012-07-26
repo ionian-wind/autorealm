bc5468751a3958341ed81cd9faf52352b9cd94f6
bmorel <berenger.morel@neutralite.org>
2012-07-26 20:53:50 +0200

    improve ID documentation and security
    Throw an exception when ID's limit is reached.
    Do not work longer with more than one type.

--- src/gui/id.cpp
+++ src/gui/id.cpp
@@ -1,25 +1,29 @@
 #include "id.h"
 
+#include <stdexcept>
+
 #include <wx/utils.h>
 uint16_t ID::s_nextID=wxNewId();
 
 ID::ID(void)
 :m_id(s_nextID)
 {
+	if(0xFFFF==s_nextID)
+		throw std::runtime_error("Can not manage more than 65536 (0xFFFF)ID.\nPlease send a bug report.");
 	++s_nextID;
 }
 
-ID::ID(unsigned long id)
+ID::ID(uint16_t id) throw()
 :m_id(id)
 {
 }
 
-const uint16_t ID::operator()(void)const
+const uint16_t ID::operator()(void)const throw()
 {
 	return m_id;
 }
 
-ID::operator int(void)const
+ID::operator uint16_t(void)const throw()
 {
 	return operator()();
 }
--- src/gui/id.h
+++ src/gui/id.h
@@ -6,13 +6,27 @@
 class ID
 {
 	public:
+        /** \brief take an unused ID number
+         *	\pre s_nextID != 0xFFFF
+         *	\post s_nextID is incremented
+         */
 		ID(void);
-		ID(unsigned long id);
-		const uint16_t operator()(void)const;
-		operator int(void)const;
+        /** \brief copy an ID number
+         *	\invariant s_nextID
+         *	\param id unsigned long
+         */
+		ID(uint16_t id) throw();
+        /** \brief get the stored number
+         * \return const uint16_t
+         */
+		const uint16_t operator()(void)const throw();
+        /** \brief cast operator to int
+         *	\return copy of m_id
+         */
+		operator uint16_t(void)const throw();
 	private:
 		static uint16_t s_nextID;
-		uint16_t m_id;
+		const uint16_t m_id;
 };
 
 #endif // ID_H
