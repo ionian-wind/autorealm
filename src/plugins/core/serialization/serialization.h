047a7b0461b308d7b8f32a46967e53da8755c478
bmorel <berenger.morel@neutralite.org>
2012-08-13 16:34:57 +0200

    added a serialization plugin

---
+++ src/plugins/core/serialization/serialization.h
@@ -0,0 +1,38 @@
+/**********************************************************************************
+ *autorealm - A vectorized graphic editor to create maps, mostly for RPG games    *
+ *Copyright (C) 2012 Morel Bérenger                                               *
+ *                                                                                *
+ *This file is part of autorealm.                                                 *
+ *                                                                                *
+ *    autorealm is free software: you can redistribute it and/or modify           *
+ *    it under the terms of the GNU Lesser General Public License as published by *
+ *    the Free Software Foundation, either version 3 of the License, or           *
+ *    (at your option) any later version.                                         *
+ *                                                                                *
+ *    autorealm is distributed in the hope that it will be useful,                *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ *    GNU Lesser General Public License for more details.                         *
+ *                                                                                *
+ *    You should have received a copy of the GNU Lesser General Public License    *
+ *    along with autorealm.  If not, see <http://www.gnu.org/licenses/>.          *
+ **********************************************************************************/
+
+#ifndef SERIALIZATION_H
+#define SERIALIZATION_H
+
+#include <pluginEngine/plugin.h>
+
+class Serialization : public Plugin
+{
+public:
+	void installEventManager(RenderWindow &target) throw() {};
+	void removeEventManager(void) throw(){};
+	Serialization();
+protected:
+private:
+};
+
+PLUMA_INHERIT_PROVIDER(Serialization, Plugin);
+
+#endif // SERIALIZATION_H
