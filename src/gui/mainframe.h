b76b3bead7d08ba674a1eeef116c222d6a56f309
bmorel <berenger.morel@neutralite.org>
2012-09-04 17:54:34 +0200

    create a typedef for std::vector<RenderWindow*>

--- src/gui/mainframe.h
+++ src/gui/mainframe.h
@@ -42,8 +42,9 @@ class MainFrame : public wxFrame
 {
 	Menu m_menuTree;	/// contain all menus and submenus of the menubar
 	///\todo avoid the use of pointers
-	std::vector<RenderWindow *> m_plans;	/// list of plans. Aka: drawing sheets
-	std::vector<RenderWindow *>::iterator m_active;	/// iterator on the currently active plan
+	typedef std::vector<RenderWindow*> MapList;
+	MapList m_plans;	/// list of plans. Aka: drawing sheets
+	MapList::iterator m_active;	/// iterator on the currently active plan
 	wxAuiManager m_auiManager;	/// internal "window manager" of wxWidgets
 	wxAuiNotebook *m_auiNotebookWorkspace;	/// contain plans'windows
 
